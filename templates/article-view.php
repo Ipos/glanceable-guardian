ac0467800a81a53aed7e07837a79e81637cda425
Tom Martin <tom@devart.org>
2009-12-08 08:14:32 +0000

    Tarted up the article view

--- templates/article-view.php
+++ templates/article-view.php
@@ -7,12 +7,24 @@
 		<li><a href="" id="article-back">X Close</a></li>
 		<li><a href="#" id="instapaper">Read Later</a> (<a href="#">What&apos;s this?</a>) </li>
 		<li><a href="#" id="twitter">Tweet this story</a></li>
-		<li><a href="#">Link</a></li>
+		<li><a href="<?= $item->web_url?>">Link</a></li>
 	</ul>
 </div>
 <div class="article">
+		
 	<h1><?=widont($item->headline);?></h1>
+	<h2 class="trail"><?=widont($item->trail_text)?></h2>
+	
+	<p class="byline"><?= $item->byline?></p>
+	
+	<div class="article-body">
 
-	<p><?=$item->type_specific['body']; ?></p>
+		<?php if ($item->trail_image_url):?>
+	 		<img src="<?= $item->trail_image_url ?>" />
+		<?php endif;?>
+	
+		<p><?=$item->type_specific['body'] ?></p>
+	
+	</div>
 	
 </div>
