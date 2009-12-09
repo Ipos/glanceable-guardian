ff0a836bbe701e5a25d03905a48a55a1a6ebf82b
deplorableword <tom@devart.org>
2009-12-09 13:14:49 +0000

    Added switch to hide errors on 'live' site

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php 
-	if (stripos($_SERVER['HTTP_HOST'], '.site')) {
-		error_reporting(E_ERROR | E_WARNING | E_PARSE);	
-	} else {
+	if(stripos($_SERVER["HTTP_HOST"], "glanceableguardian.nfshost.com") !== FALSE){
+		$status = "live";
 		error_reporting(0);
+	}elseif(stripos($_SERVER["HTTP_HOST"], "glanceableguardianstaging.nfshost.com") !== FALSE){
+		$status = "staging";
+		error_reporting(E_ERROR | E_WARNING | E_PARSE);		
+	}else{
+		$status = "development";
+		error_reporting(E_ERROR | E_WARNING | E_PARSE);		
 	}
-	
+
 	include('./lib/lastRSS.php');
 	include('./lib/functions.php');	
 	
